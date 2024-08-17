98a418f0e58998fe41c21da86870e6ad098d84fd
Pramod Kumar <pramodk935@gmail.com>
2024-08-17 07:59:13 +0000

    Added test dependencies and simple unit test

---
+++ src/test/java/hello-world-web.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class HelloWorldTest {
+    @Test
+    public void testHelloWorld() {
+        // Simple test to ensure the test framework is working
+        assertTrue("Test framework is working", true);
+    }
+}
